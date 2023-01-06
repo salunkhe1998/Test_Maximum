adbe10f664b13441507fb404d5c4befa7d8c7b37
Ashwini <salunkheashwini1998@gmail.com>
2023-01-06 18:54:43 +0530

    this is file

--- FindMaximum.cs
+++ FindMaximum.cs
@@ -8,7 +8,7 @@ namespace TestMaximum
 {
     public class FindMax<T> where T : IComparable
     {
-        public static T MaxFloatNumber(T X, T Y, T Z)
+        public static T MaxOfString(T X, T Y, T Z)
         {
             if (X.CompareTo(Y) > 0 && X.CompareTo(Z) > 0 ||
                 X.CompareTo(Y) >= 0 && X.CompareTo(Z) > 0 ||
