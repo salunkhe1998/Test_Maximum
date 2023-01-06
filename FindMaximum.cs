6fd2db4cfd24ba53843132b5d83f702a1148d7da
Ashwini <salunkheashwini1998@gmail.com>
2023-01-06 19:03:44 +0530

    this is file

--- FindMaximum.cs
+++ FindMaximum.cs
@@ -6,9 +6,10 @@ using System.Threading.Tasks;
 
 namespace TestMaximum
 {
+
     public class FindMax<T> where T : IComparable
     {
-        public static T MaxOfString(T X, T Y, T Z)
+        public static T MaxOfIntNum(T X, T Y, T Z)
         {
             if (X.CompareTo(Y) > 0 && X.CompareTo(Z) > 0 ||
                 X.CompareTo(Y) >= 0 && X.CompareTo(Z) > 0 ||
@@ -34,6 +35,62 @@ namespace TestMaximum
                 return Z;
             }
             return X;
+        }
+
+        public static T MaxOfFloatNum(T A, T B, T C)
+        {
+            if (A.CompareTo(B) > 0 && A.CompareTo(C) > 0 ||
+                A.CompareTo(B) >= 0 && A.CompareTo(C) > 0 ||
+                A.CompareTo(B) > 0 && A.CompareTo(C) >= 0)
+
+            {
+                return A;
+            }
+
+            if (B.CompareTo(A) > 0 && B.CompareTo(C) > 0 ||
+                B.CompareTo(A) >= 0 && B.CompareTo(C) > 0 ||
+                B.CompareTo(A) > 0 && B.CompareTo(C) >= 0)
+
+            {
+                return B;
+            }
+
+            if (C.CompareTo(A) > 0 && C.CompareTo(B) > 0 ||
+                C.CompareTo(A) >= 0 && C.CompareTo(B) > 0 ||
+                C.CompareTo(A) > 0 && C.CompareTo(B) >= 0)
+
+            {
+                return C;
+            }
+            return A;
+        }
+
+        public static T MaxOfString(T L, T M, T N)
+        {
+            if (L.CompareTo(M) > 0 && L.CompareTo(N) > 0 ||
+                L.CompareTo(M) >= 0 && L.CompareTo(N) > 0 ||
+                L.CompareTo(M) > 0 && L.CompareTo(N) >= 0)
+
+            {
+                return L;
+            }
+
+            if (M.CompareTo(L) > 0 && M.CompareTo(N) > 0 ||
+                M.CompareTo(L) >= 0 && M.CompareTo(N) > 0 ||
+                M.CompareTo(L) > 0 && M.CompareTo(N) >= 0)
+
+            {
+                return M;
+            }
+
+            if (N.CompareTo(L) > 0 && N.CompareTo(M) > 0 ||
+                N.CompareTo(L) >= 0 && N.CompareTo(M) > 0 ||
+                N.CompareTo(L) > 0 && N.CompareTo(M) >= 0)
+
+            {
+                return N;
+            }
+            return L;
         }
     }
 }
